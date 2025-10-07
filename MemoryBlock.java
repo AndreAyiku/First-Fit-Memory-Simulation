0392a36c634d3d48e89a5bceeb13c5db619f8dbf
jadarko55 <jasuno55@gmail.com>
2025-10-07 20:24:31 +0000

    Added block-level utilization counters: timeUsedTicks: increments each tick when the block is occupied. timesAssigned: increments each time a job is loaded into the block. maxUtilization: tracks the max percentage of block size utilized by any job seen. loadJob(): increments timesAssigned and updates maxUtilization for the assigned job. processJob(): increments timeUsedTicks on each tick the block is occupied.

--- MemoryBlock.java
+++ MemoryBlock.java
@@ -3,6 +3,10 @@ public class MemoryBlock {
     public int blockSize;           // How big this block is
     public Job currentJob;          // Job using this block (
     public boolean isEmpty;         
+    // Metrics
+    public int timeUsedTicks;       // how many ticks this block was occupied
+    public int timesAssigned;       // how many jobs have been assigned to this block
+    public int maxUtilization;      // track max utilization percent seen (0-100)
     
     
     public MemoryBlock(int number, int size) { //constructor 
@@ -10,6 +14,9 @@ public class MemoryBlock {
         this.blockSize = size;
         this.currentJob = null;
         this.isEmpty = true;
+        this.timeUsedTicks = 0;
+        this.timesAssigned = 0;
+        this.maxUtilization = 0;
     }
     public boolean canFit(Job job) { //checks to see if a job can enter a memoryBlock
         if (isEmpty && job.memoryNeeded <= blockSize){ // if the block is empty and the jobs memory is less that the block size, it returns true
@@ -22,6 +29,10 @@ public class MemoryBlock {
         this.currentJob = job;
         this.isEmpty = false;
         job.start(currentTime, this.blockNumber);
+        this.timesAssigned++;
+        // update max utilization
+        int util = (int)Math.round((job.memoryNeeded * 100.0) / blockSize);
+        if (util > maxUtilization) maxUtilization = util;
     }
     public void unloadJob() { //deallocates a job from memory
         if (currentJob != null) {
@@ -39,6 +50,7 @@ public class MemoryBlock {
     public boolean processJob() { //uses the tick method to process the job for a unit of time
         if (!isEmpty && currentJob != null) {
             currentJob.tick();
+            this.timeUsedTicks++; // count utilization time
             
             // If job finished, unload it
             if (currentJob.isDone()) {
